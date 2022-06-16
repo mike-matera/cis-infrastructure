c621b6efbc3ccd5a13480e34677221e5b019b8c3
Mike Matera <matera@lifealgorithmic.com>
2022-06-16 04:32:40 +0000

    Don't start the container on validation failure.

--- apps/DNS/README.md
+++ apps/DNS/README.md
@@ -9,3 +9,17 @@ Load the config map from the file in hosts:
 ```
 kubectl -n cis-dns create configmap cis-dns --from-file=hosts/10-inventory.yaml
 ```
+
+## Updating the DNS configuration 
+
+First update the configmap:
+
+```console
+kubectl create configmap cis-dns --from-file=hosts/10-inventory.yaml -o yaml --dry-run=client | kubectl -n cis-dns apply -f  - 
+```
+
+Then roll the deployment: 
+
+```console
+kubectl -n cis-dns rollout restart deployment cis-dns-external cis-dns-internal 
+```
--- apps/DNS/cis-dns/entrypoint.sh
+++ apps/DNS/cis-dns/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ -z "$DNS_ROLE" ]; then
     echo "ERROR!"
-    echo "You must specify a value fro \$DNS_ROLE, either internal or external"
+    echo "You must specify a value for \$DNS_ROLE, either internal or external"
     exit -1 
 fi 
 
@@ -13,4 +13,13 @@ cp build/db.* /etc/bind/
 chown -R bind:bind /etc/bind
 
 cp named-$DNS_ROLE.conf /etc/bind/named-$DNS_ROLE.conf 
+
+(
+    set -e
+    /usr/sbin/named -g -c /etc/bind/named-$DNS_ROLE.conf -u bind &
+    sleep 2
+    python3 ./test.py 
+    kill $$
+)
+
 exec /usr/sbin/named -g -c /etc/bind/named-$DNS_ROLE.conf -u bind 
